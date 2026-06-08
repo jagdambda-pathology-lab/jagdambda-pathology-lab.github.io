746dacda3c058d6765dd8e88087928a19518a740
bikram <bikcrum@gmail.com>
2026-06-08 13:15:33 +0545

    Updates

---
+++ precache-manifest.1d43c73218475999fa418c6002ab86e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a72035c6974a7d9504791bc72bb1a39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81c9593144b43fd41ee3",
+    "url": "/static/css/2.a4a6e74e.chunk.css"
+  },
+  {
+    "revision": "996b9952099f71097fd8",
+    "url": "/static/css/main.3d5603eb.chunk.css"
+  },
+  {
+    "revision": "81c9593144b43fd41ee3",
+    "url": "/static/js/2.1f3b8ebf.chunk.js"
+  },
+  {
+    "revision": "9542eaf46166fb164d00e7baf1ceab77",
+    "url": "/static/js/2.1f3b8ebf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "996b9952099f71097fd8",
+    "url": "/static/js/main.e1431fd8.chunk.js"
+  },
+  {
+    "revision": "7052caab4ac51b6d0059",
+    "url": "/static/js/runtime-main.0d25201a.js"
+  }
+]);
